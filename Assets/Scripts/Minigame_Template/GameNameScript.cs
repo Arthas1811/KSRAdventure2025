0ce1e4934fba7d4a3b515cf7f18bcbf9eab79957
Lesieber <lesieber@ksr.ch>
2026-01-22 22:50:16 +0100

    adding locally merged files/changing file structure

---
+++ Assets/Scripts/Minigame_Template/GameNameScript.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json.Linq;
+
+public class GameNameScript : MonoBehaviour
+{
+    public Inventory inventory;
+    public RawImage background;
+    JObject saveData;
+    public SaveDataManager saveDataManager;
+    void Start()
+    {
+        saveData = saveDataManager.readData();
+        GameObject obj = GameObject.Find("Background");
+        background = obj.GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true){// repace with actual if statement
+            Win();
+        }
+        else
+        {
+            Lose();
+        }
+    }
+    void Win()
+    {
+        inventory.add("redbull"); // evt. add item to inventory
+ 
+        // Savedata changes
+        saveData["states"]["start"]["openDoor"] = true; // start -> place,
+        saveDataManager.saveData(saveData);
+ 
+        //switch scene
+        SceneManager.LoadScene("main");
+    }
+ 
+    void Lose()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
